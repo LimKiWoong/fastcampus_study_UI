16dc4a59bdd1a863f00667b887db42611566d477
LimKiWoong <lkw4670@naver.com>
2021-11-30 18:21:43 +0900

    study

--- app/src/main/java/com/woongs/fastclass_study_ui/FragmentActivity.kt
+++ app/src/main/java/com/woongs/fastclass_study_ui/FragmentActivity.kt
@@ -3,11 +3,48 @@ package com.woongs.fastclass_study_ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_fragment.*
 
-class FragmentActivity : AppCompatActivity() {
+class FragmentActivity : AppCompatActivity(), FragmentOne.OnDataPassListener {
+
+    override fun onDataPass(data: String?) {
+        Log.d("pass",""+data)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+
+        val fragmentOne : FragmentOne = FragmentOne()
+        // 프레그먼트에 data를 넣어주는 방법 -> bundle
+        val bundle : Bundle = Bundle()
+        bundle.putString("hello","hello")
+        fragmentOne.arguments = bundle
+
+
+        button.setOnClickListener {
+            val fragmentManager : FragmentManager = supportFragmentManager
+
+            //Transaction
+            // 작업의 단위 -> 시작과 끝이 있다
+            // 시작
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container,fragmentOne)
+            // 종료
+            fragmentTransaction.commit()
+            // 종료 하는 방법
+            // commit    -> 시간될때  (좀더 안정적)
+            // commitnow -> 지금당장
+        }
+        button2.setOnClickListener {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.remove(fragmentOne)
+            fragmentTransaction.commit()
+            // detach
+            // remove
+        }
 
         Log.d("life_cycle","onCreate")
     }
--- app/src/main/java/com/woongs/fastclass_study_ui/FragmentOne.kt
+++ app/src/main/java/com/woongs/fastclass_study_ui/FragmentOne.kt
@@ -7,12 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_one.*
 
 class FragmentOne : Fragment(){
+
+    interface OnDataPassListener {
+        fun onDataPass(data : String?)
+    }
+
+    lateinit var dataPassListener : OnDataPassListener
 
     override fun onAttach(context: Context) {
         Log.d("life_cycle","F_onAttach")
         super.onAttach(context)
+        dataPassListener = context as OnDataPassListener
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,13 +40,23 @@ class FragmentOne : Fragment(){
         return inflater.inflate(R.layout.fragment_one,container,false)
     }
 
+    // Activity의 onCreate에서 했던 작업을 Fragment는 주로 이곳에 작업한다
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Log.d("life_cycle","F_onViewCreated")
         super.onViewCreated(view, savedInstanceState)
+
+        pass.setOnClickListener {
+            dataPassListener.onDataPass("Good Bye")
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         Log.d("life_cycle","F_onActivityCreated")
+        val data = arguments?.getString("hello")
+        if (data != null) {
+            Log.d("data",data)
+        }
         super.onActivityCreated(savedInstanceState)
     }
 
