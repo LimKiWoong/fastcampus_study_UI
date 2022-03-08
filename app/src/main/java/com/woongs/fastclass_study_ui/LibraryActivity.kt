a141b1ed37f6bf7656e0164efe0422492aabbb8e
LimKiWoong <lkw4670@naver.com>
2022-03-08 16:37:50 +0900

    study

--- app/src/main/java/com/woongs/fastclass_study_ui/LibraryActivity.kt
+++ app/src/main/java/com/woongs/fastclass_study_ui/LibraryActivity.kt
@@ -2,10 +2,17 @@ package com.woongs.fastclass_study_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_library.*
 
 class LibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
+
+        Glide.with(this)
+            .load(R.drawable.ic_launcher_background)
+            .centerCrop()
+            .into(imageview)
     }
 }
